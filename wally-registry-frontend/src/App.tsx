075f99610efc9c5c7f81affd7bc7380742216e94
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-10-20 12:02:04 -0500

    Route Search and Package pages

--- wally-registry-frontend/src/App.tsx
+++ wally-registry-frontend/src/App.tsx
@@ -12,8 +12,9 @@ import MDXComponents from "./MDXComponents"
 import Home from "./pages/Home"
 import Install from "./pages/Install.mdx"
 import NotFound from "./pages/NotFound"
-// import Package from "./pages/Package"
+import Package from "./pages/Package"
 import Policies from "./pages/Policies.mdx"
+import Search from "./pages/Search"
 
 export default function App() {
   return (
@@ -43,13 +44,13 @@ export default function App() {
               <Home />
             </Route>
 
-            {/* <Route path="/search">
+            <Route path="/search">
               <Search />
             </Route>
 
             <Route path="/package/:packageSlug">
               <Package />
-            </Route> */}
+            </Route>
 
             <Route path="/install">
               <Helmet>
