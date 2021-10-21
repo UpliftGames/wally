6028a6c8913ea6936c90b48293a04a181f9e9004
magnalite <jonny@uplift.games>
2021-10-21 19:05:10 +0100

    Add --list functionality to package command

--- src/commands/package.rs
+++ src/commands/package.rs
@@ -12,15 +12,29 @@ pub struct PackageSubcommand {
     #[structopt(long = "project-path", default_value = ".")]
     pub project_path: PathBuf,
 
+    /// Output a list of files which would be included in the package instead
+    /// of creating the package
+    #[structopt(short, long)]
+    pub list: bool,
+
     /// Output file path where the package will be created
-    #[structopt(long = "output")]
-    pub output_path: PathBuf,
+    #[structopt(long = "output", required_unless("list"))]
+    pub output_path: Option<PathBuf>,
 }
 
 impl PackageSubcommand {
     pub fn run(self) -> anyhow::Result<()> {
-        let contents = PackageContents::pack_from_path(&self.project_path)?;
-        fs_err::write(&self.output_path, contents.data())?;
+        if self.list {
+            let contents_list = PackageContents::filtered_contents(&self.project_path)?;
+
+            for path in contents_list {
+                println!("{}", path.display());
+            }
+        } else {
+            let contents = PackageContents::pack_from_path(&self.project_path)?;
+            fs_err::write(&self.output_path.unwrap(), contents.data())?;
+        }
+
         Ok(())
     }
 }
