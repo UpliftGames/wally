fd1523331ddd35fd72140287513452738059a056
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-10-22 14:23:17 -0500

    Integrate Wally backend repo API

---
+++ wally-registry-frontend/src/services/wally.api.ts
@@ -0,0 +1,25 @@
+const wallyApiBaseUrl = "https://api.test.wally.run/v1"
+// https://api.test.wally.run/v1/package-search/<query>
+const wallyApiSearchUrl = `${wallyApiBaseUrl}/package-search`
+// https://api.test.wally.run/v1/package-metadata/<scope>/<name>
+const wallyApiMetadataUrl = `${wallyApiBaseUrl}/package-metadata`
+
+export async function getWallyPackages(searchQuery: string | null) {
+  if (searchQuery && searchQuery.length > 1) {
+    // return fetch(`${wallyApiSearchUrl}/${searchQuery}`)
+    return fetch("https://api.test.wally.run/v1/package-search/ro")
+      .then((response) => response.json())
+      .then((data) => data)
+  } else {
+    return []
+  }
+}
+
+export async function getWallyPackageMetadata(
+  packageScope: string,
+  packageName: string
+) {
+  return fetch(`${wallyApiMetadataUrl}/${packageScope}/${packageName}`)
+    .then((response) => response.json())
+    .then((data) => data.versions[0])
+}
