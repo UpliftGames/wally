e889450a596774a80c266d98a3c00b73329f5ae5
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2021-10-25 12:48:31 -0500

    Add doc comments to Wally Api functions

--- wally-registry-frontend/src/services/wally.api.ts
+++ wally-registry-frontend/src/services/wally.api.ts
@@ -1,9 +1,17 @@
+import { WallyPackageBrief, WallyPackageMetadata } from "../types/wally"
+
 const wallyApiBaseUrl = "https://api.test.wally.run/v1"
 // https://api.test.wally.run/v1/package-search/<query>
 const wallyApiSearchUrl = `${wallyApiBaseUrl}/package-search`
 // https://api.test.wally.run/v1/package-metadata/<scope>/<name>
 const wallyApiMetadataUrl = `${wallyApiBaseUrl}/package-metadata`
 
+/**
+ * Fetches a list of packages from Wally. The search string is matched against the package scope, name, and description of all available packages
+ * A specific field can be matched against by prefixing the searchQuery with the name of that field. Ex: "description: ui"
+ * @param {string} searchQuery - The search query as a series of characters
+ * @returns {WallyPackageBrief[]} The list of Wally shorthand descriptions that match the searchQuery
+ */
 export async function getWallyPackages(searchQuery: string | null) {
   if (searchQuery && searchQuery.length > 1) {
     return fetch(`${wallyApiSearchUrl}/${searchQuery}`)
@@ -14,6 +22,12 @@ export async function getWallyPackages(searchQuery: string | null) {
   }
 }
 
+/**
+ * Fetches a single package from Wally with all it's corresponding meta information
+ * @param {string} packageScope - The owning author or organization of the package
+ * @param {string} packageName - The search query as a series of characters
+ * @returns {WallyPackageMetadata} The package with it's associated data and dependencies
+ */
 export async function getWallyPackageMetadata(
   packageScope: string,
   packageName: string
