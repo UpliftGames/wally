69ba6f271d6ff335ed1b7e4b8c169888ce5b4618
Syntheticdev <marineboy308@gmail.com>
2022-06-11 23:09:35 +0100

    fallback search v1

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -24,33 +24,61 @@ pub struct SearchSubcommand {
 impl SearchSubcommand {
     pub fn run(self) -> anyhow::Result<()> {
         let manifest = Manifest::load(&self.project_path)?;
-        let registry = url::Url::parse(&manifest.package.registry)?;
         let auth_store = AuthStore::load()?;
-        let package_index = PackageIndex::new(&registry, None)?;
-        let api = package_index.config()?.api;
 
-        let auth = auth_store.tokens.get(api.as_str());
+        let mut package_indexes: Vec<PackageIndex> = Vec::new();
+        let mut registry_order = vec![manifest.package.registry];
+        let mut registry_index = 0;
+
+        while registry_index < registry_order.len() {
+            let registry = &registry_order[registry_index];
+            let url = url::Url::parse(&registry)?;
+            let package_index = PackageIndex::new(&url, None)?;
+            let fallback_registries = package_index.config()?.fallback_registries;
+
+            package_indexes.push(package_index);
+
+            for fallback in fallback_registries {
+                // Prevent circular references by only adding new registries
+                if !registry_order.contains(&&fallback) {
+                    registry_order.push(fallback);
+                }
+            }
+
+            registry_index += 1;
+        }
 
         let client = Client::new();
-        let mut request = client
-            .get(api.join("/v1/package-search/")?)
-            .query(&[("query", &self.query)]);
+        let mut results: Vec<SearchResult> = Vec::new();
 
-        if let Some(auth) = auth {
-            request = request.header(AUTHORIZATION, format!("Bearer {}", auth));
+        for package_index in package_indexes {
+            log::info!("Searching package index {}...", package_index.url());
+
+            let api = package_index.config()?.api;
+            let auth = auth_store.tokens.get(api.as_str());
+
+            let mut request = client
+                .get(api.join("/v1/package-search/")?)
+                .query(&[("query", &self.query)]);
+
+            if let Some(auth) = auth {
+                request = request.header(AUTHORIZATION, format!("Bearer {}", auth));
+            }
+
+            let response = request.send()?;
+
+            if !response.status().is_success() {
+                bail!(
+                    "Failed to search: {} {}",
+                    response.status(),
+                    response.text()?
+                );
+            }
+
+            let package_results: Vec<SearchResult> = response.json()?;
+            results.extend(package_results);
         }
 
-        let response = request.send()?;
-
-        if !response.status().is_success() {
-            bail!(
-                "Failed to search: {} {}",
-                response.status(),
-                response.text()?
-            );
-        }
-
-        let mut results: Vec<SearchResult> = response.json()?;
         println!();
 
         for result in &mut results {
