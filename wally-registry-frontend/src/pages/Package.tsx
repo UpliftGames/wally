42e8f5c667ded0e65396ecb74e45bdaedb547983
MobiusCraftFlip <developmentmobius@gmail.com>
2023-06-11 10:19:32 +0100

    fix incorrect package version sorting

--- wally-registry-frontend/src/pages/Package.tsx
+++ wally-registry-frontend/src/pages/Package.tsx
@@ -205,7 +205,7 @@ export default function Package() {
       ? packageData.versions.filter(
           (pack: WallyPackageMetadata) => !pack.package.version.includes("-")
         )
-      : packageData
+      : packageData.versions
 
     setPackageHistory(filteredPackageData)
 
