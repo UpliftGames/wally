0f1f703c95138bc80f8f268daa2e713724ab71ea
utrain <skassem05@outlook.com>
2024-11-02 00:07:46 -0400

    added unit tests for diffing upgrades

--- src/commands/utils.rs
+++ src/commands/utils.rs
@@ -1,12 +1,14 @@
 use crate::{package_id::PackageId, package_name::PackageName};
 use crossterm::style::{Color, SetForegroundColor};
+use serde::Serialize;
 use std::{collections::BTreeSet, io::Write};
 
+#[derive(Debug, Eq, PartialEq, Serialize)]
 pub(crate) enum DependencyChange {
     Added(PackageId),
     Removed(PackageId),
     Updated { from: PackageId, to: PackageId },
-    Downgrade { from: PackageId, to: PackageId },
+    Downgraded { from: PackageId, to: PackageId },
 }
 
 pub(crate) fn generate_dependency_changes(
@@ -42,7 +44,7 @@ pub(crate) fn generate_dependency_changes(
                     (Some(old), Some(new)) if old.le(&new) => {
                         DependencyChange::Updated { from: old, to: new }
                     }
-                    (Some(old), Some(new)) => DependencyChange::Downgrade { from: old, to: new },
+                    (Some(old), Some(new)) => DependencyChange::Downgraded { from: old, to: new },
 
                     // Or, there's been a singular removal/addition.
                     (Some(old), None) => DependencyChange::Removed(old),
@@ -109,7 +111,7 @@ pub(crate) fn render_update_difference(
                 from.version(),
                 to.version()
             ),
-            DependencyChange::Downgrade { from, to } => writeln!(
+            DependencyChange::Downgraded { from, to } => writeln!(
                 writer,
                 "{} Downgraded {}{} from v{} to v{}",
                 SetForegroundColor(Color::DarkYellow),
@@ -122,4 +124,164 @@ pub(crate) fn render_update_difference(
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::{collections::BTreeSet, str::FromStr};
+
+    use super::{generate_dependency_changes, render_update_difference};
+    use insta::assert_snapshot;
+
+    macro_rules! package_id {
+        ($package_id:literal) => {
+            crate::package_id::PackageId::from_str($package_id).unwrap()
+        };
+    }
+
+    macro_rules! added_change {
+        ($changes:expr, $package_id:literal) => {
+            $changes
+                .iter()
+                .any(|change| *change == super::DependencyChange::Added(package_id!($package_id)))
+        };
+    }
+
+    macro_rules! removed_change {
+        ($changes:expr, $package_id:literal) => {
+            $changes
+                .iter()
+                .any(|change| *change == super::DependencyChange::Removed(package_id!($package_id)))
+        };
+    }
+
+    macro_rules! upgraded_change {
+        ($changes:expr, $from_package_id:literal, $to_package_id:literal) => {
+            $changes.iter().any(|change| {
+                *change
+                    == super::DependencyChange::Updated {
+                        from: package_id!($from_package_id),
+                        to: package_id!($to_package_id),
+                    }
+            })
+        };
+    }
+
+    macro_rules! downgraded_change {
+        ($changes:expr, $from_package_id:literal, $to_package_id:literal) => {
+            $changes.iter().any(|change| {
+                *change
+                    == super::DependencyChange::Downgraded {
+                        from: package_id!($from_package_id),
+                        to: package_id!($to_package_id),
+                    }
+            })
+        };
+    }
+
+    #[test]
+    fn generate_no_changes_if_same() {
+        let dependencies = BTreeSet::from([
+            package_id!("biff/package-a@1.1.1"),
+            package_id!("biff/package-b@1.2.1"),
+        ]);
+
+        let changes = generate_dependency_changes(&dependencies, &dependencies);
+
+        assert!(changes.is_empty(), "Expected no changes.")
+    }
+
+    #[test]
+    fn generate_correct_changes() {
+        let old_dependencies = BTreeSet::from([
+            package_id!("biff/unchanged-package@1.0.0"),
+            package_id!("biff/removed-package@1.2.1"),
+            package_id!("biff/upgraded-package@1.2.1"),
+            package_id!("biff/downgraded-package@1.2.1"),
+        ]);
+
+        let new_dependencies = BTreeSet::from([
+            package_id!("biff/unchanged-package@1.0.0"),
+            package_id!("biff/added-package@3.1.1"),
+            package_id!("biff/upgraded-package@1.2.4"),
+            package_id!("biff/downgraded-package@0.0.1"),
+        ]);
+
+        let changes = generate_dependency_changes(&old_dependencies, &new_dependencies);
+
+        assert!(!changes.is_empty(), "Expected changes.");
+        assert!(changes.len() == 4, "Expected four changes.");
+        assert!(
+            added_change!(changes, "biff/added-package@3.1.1"),
+            "Expected biff/added-package to be added."
+        );
+        assert!(
+            removed_change!(changes, "biff/removed-package@1.2.1"),
+            "Expected biff/remove-package to be removed."
+        );
+        assert!(
+            upgraded_change!(
+                changes,
+                "biff/upgraded-package@1.2.1",
+                "biff/upgraded-package@1.2.4"
+            ),
+            "Expected biff/upgraded-package to be upgraded."
+        );
+        assert!(
+            downgraded_change!(
+                changes,
+                "biff/downgraded-package@1.2.1",
+                "biff/downgraded-package@0.0.1"
+            ),
+            "Expected biff/downgraded-package to be downgraded."
+        );
+    }
+
+    #[test]
+    fn decompose_upgrades_and_downgrades_if_multiple() {
+        let old_dependencies = BTreeSet::from([
+            package_id!("biff/package-a@1.0.0"),
+            package_id!("biff/package-b@1.0.0"),
+        ]);
+
+        let new_dependencies = BTreeSet::from([
+            package_id!("biff/package-a@2.0.0"),
+            package_id!("biff/package-a@3.0.0"),
+            package_id!("biff/package-b@0.5.0"),
+            package_id!("biff/package-b@0.5.1"),
+        ]);
+
+        let changes = generate_dependency_changes(&old_dependencies, &new_dependencies);
+
+        assert!(!changes.is_empty(), "Expected changes.");
+        assert!(changes.len() == 6, "Expected only six changes.");
+        assert!(
+            removed_change!(changes, "biff/package-a@1.0.0"),
+            "Expected package-a@1.0.0 to be removed."
+        );
+        assert!(
+            removed_change!(changes, "biff/package-b@1.0.0"),
+            "Expected package-b@1.0.0 to be removed."
+        );
+        assert!(
+            added_change!(changes, "biff/package-a@2.0.0")
+                && added_change!(changes, "biff/package-a@3.0.0"),
+            "Upgrades decomposed."
+        );
+        assert!(
+            added_change!(changes, "biff/package-b@0.5.0")
+                && added_change!(changes, "biff/package-b@0.5.1"),
+            "Upgrades decomposed."
+        )
+    }
+
+    #[test]
+    fn snapshot_output_when_no_changes() {
+        let changes = Vec::new();
+
+        let mut writer = Vec::new();
+        render_update_difference(&changes, &mut writer).unwrap();
+
+        assert_snapshot!(String::from_utf8(writer).unwrap());
+    }
 }
