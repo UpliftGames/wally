b7825ba229b175523abea0718fd346c1d8567dba
Riley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2024-11-07 15:25:34 -0800

    Add Install page

--- wally-registry-frontend/src/app/install/page.tsx
+++ wally-registry-frontend/src/app/install/page.tsx
@@ -1,5 +1,15 @@
+"use client"
+
+import ContentSection from "@/components/ContentSection"
+import { MarkdownSection } from "@/components/MarkdownSection"
 import Install from "@/markdown/Install.mdx"
 
 export default function InstallPage() {
-  return <Install />
+  return (
+    <ContentSection>
+      <MarkdownSection>
+        <Install />
+      </MarkdownSection>
+    </ContentSection>
+  )
 }
