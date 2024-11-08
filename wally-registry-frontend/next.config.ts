0ff6b3485b1e344f5bb965c1d939ab32a6069e69
Riley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2024-11-07 16:34:22 -0800

    Update next.config.ts

--- wally-registry-frontend/next.config.ts
+++ wally-registry-frontend/next.config.ts
@@ -2,7 +2,6 @@ import createMDX from "@next/mdx"
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: "export",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   compiler: {
     styledComponents: true,
