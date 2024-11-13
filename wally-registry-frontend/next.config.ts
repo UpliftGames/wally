5450eb274f772ed6bd06bd62820431b1b8d2beaa
UpliftMacaw <martin@uplift.games>
2024-11-13 14:16:03 -0600

    Deploy to Cloudflare Pages (#208)
    * Build on Cloudflare Pages
    
    * Use Node 22 LTS
    
    * Use Node 20.x
    
    * Remove engine specification
    
    * Use engine specification
    
    * Try and nvmrc instead
    
    * Assign vars in wrangler.toml

--- wally-registry-frontend/next.config.ts
+++ wally-registry-frontend/next.config.ts
@@ -1,5 +1,6 @@
 import createMDX from "@next/mdx"
 import type { NextConfig } from "next"
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
@@ -11,5 +12,9 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
 })
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default withMDX(nextConfig)
